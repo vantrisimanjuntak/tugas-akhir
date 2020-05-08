4968f0ea74c24bcd2ba0b2b9f21e2522a8762ad7
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-05-09 00:27:25 +0700

    lecture function

---
+++ application/modules/lecture/models/Model_lecture.php
@@ -0,0 +1,10 @@
+<?php class Model_lecture extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function login()
+    {
+    }
+}
--- application/modules/lecture/views/lecture_view.php
+++ application/modules/lecture/views/lecture_view.php
@@ -4,17 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('bootstrap-4.0.0/dist/css/bootstrap.min.css') ?>">
     <!-- Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=PT+Serif&display=swap" rel="stylesheet">
+    <link href=" https://fonts.googleapis.com/css2?family=Bree+Serif&family=PT+Serif&display=swap" rel="stylesheet">
     <!-- End Font -->
     <!-- Shortcut Icon -->
     <link rel="shortcut icon" href="<?= base_url('assets/images/stta.png'); ?>">
     <!-- Added JS -->
     <script src="<?= base_url('assets/js/app.js') ?>"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/javascript/jquery-3.5.1.js'); ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="<?= base_url('bootstrap-4.0.0/dist/js/bootstrap.min.js') ?>"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <title><?= $title; ?></title>
 </head>
