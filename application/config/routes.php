c37f73f1f435c5135b6a3011e44eb74975f6ca22
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-05-09 20:19:06 +0700

    update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $route['default_controller'] = 'dashboard/home';
 $route['lecture'] = 'lecture/lecture';
-$route['tugas_akhir'] = 'tugas_akhir';
+$route['skripsi'] = 'skripsi/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
