88b41564c8657ec4f2087a603e24291ca8557b25
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-07-06 00:42:18 +0700

    Change DB

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ta',
+	'hostname' => 'sql300.epizy.com ',
+	'username' => 'epiz_26104039',
+	'password' => 'wDA6rstA3DqiQ3L',
+	'database' => 'epiz_26104039_ta',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
