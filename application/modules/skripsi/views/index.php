6dc37b7581d05d98408d7113e0dc4a1f1cd1435a
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-07-06 19:46:06 +0700

    Personalize Skripsi page

--- application/modules/skripsi/views/index.php
+++ application/modules/skripsi/views/index.php
@@ -28,28 +28,11 @@
 
 <body style="background:linear-gradient(to top, #66ffff 19%, #ff99cc 97%)">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #3104B4">
-        <a class="navbar-brand" href="#">
-            <img src="<?= base_url('assets/images/stta.png') ?>" class="img-fluid" style="width:40px;" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Beranda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Departemen</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php $this->load->view('nav'); ?>
     <!-- End Navbar -->
 
     <!-- Main Content -->
-    <div class="container mt-3 pb-5" style="border-style: ridge; background-color:white">
+    <div class="container pb-5" style="margin-top: 70px;border-style: ridge; background-color:white">
         <form action="<?= base_url('skripsi/home/submit'); ?>" method="post" id="submitSkripsi">
             <div class="container border-dark">
                 <h4 class="font-weight-bold mt-3" style="font-family: 'Neuton', serif;">Form Input Skripsi</h4>
@@ -115,59 +98,7 @@
     <!-- End Main Content -->
 
     <!-- Footer -->
-    <div class="container-fluid mt-5" style="background-color: #81F7F3; font-family : 'Lateef', cursive; color:black ">
-        <div class="p-3">
-            <div class="row">
-                <!-- Logo -->
-                <div class="p-4 col-xs-12 col-sm-12 col-md-12 col-lg-1 col-xl-1">
-                    <img src="<?= base_url('assets/images/stta.png') ?>" class="img-fluid mx-auto d-block" width="140px;" alt="">
-                </div>
-                <!-- Nama Instansi -->
-                <div class="p-4 col-sm-12 text-center col-md-5 col-lg-3 ">
-                    <h3 class=" display-5 align-middle">Sekolah Tinggi Teknologi Adisutjipto</h3>
-                    Jl. Janti, Blok R - Lanud Adisutjipto
-                    Yogyakarta
-                </div>
-                <!-- Departemen -->
-                <div class="col-sm-6 col-sm-4 col-md-4 col-lg-2">
-                    <h3>Program Studi</h3>
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;Departemen Dirgantara</i> <br>
-
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;Departemen Informatika</i><br>
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;Teknik Elektro</i><br>
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;Departemen Industri</i><br>
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;Teknik Mesin</i> <br>
-                    <i class="fa fa-caret-right" aria-hidden="true">&nbsp;D-3 Aeronautika</i>
-                </div>
-                <!-- Sosial Media -->
-                <div class="col-sm-6 col-md-3 col-lg-2">
-                    <h3>Media Sosial</h3>
-                    <a href="#" style="text-decoration: none; color:black ">
-                        <i class="fa fa-facebook-square" aria-hidden="true"></i> &nbsp;Sekolah Tinggi Teknologi Adisutjipto <br>
-                    </a>
-                    <a href="#" style="text-decoration: none; color: black">
-                        <i class=" fa fa-instagram" aria-hidden="true">&nbsp; stt_adisutjipto</i><br>
-                    </a>
-                    <a href="#" style="text-decoration: none; color: black ">
-                        <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; STT Adisutjipto</i>
-                    </a>
-                </div>
-                <!-- Hubung Kami -->
-                <div class="col-sm-6 col-md-6 col-lg-2 align-center ">
-                    <h3>Hubungi Kami</h3>
-                    <i class="fa fa-phone " aria-hidden="true">&nbsp; +62 274 451262 (Hunting) </i> <br>
-                    <i class="fa fa-phone" aria-hidden="true">&nbsp; +62 274 451263 (Telp) </i><br>
-                    <i class="fa fa-fax" aria-hidden="true">&nbsp; +62274451265 (Fax)</i><br>
-                    <i class="fa fa-envelope-o" aria-hidden="true">&nbsp; admin@stta.ac.id</i>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid" style="background-color:#0080FF; color:white ">
-        <div class="container text-center" style="padding: 7px;font-family: 'Esteban', serif ">
-            Copyright 2010 - <?= date('Y'); ?> Sekolah Tinggi Teknologi Adisutijpto Yogyakarta. Powered by TIK STTA. All Right Reserved
-        </div>
-    </div>
+    <?php $this->load->view('footer'); ?>
     <!-- End Footer -->
 </body>
 
---
+++ application/views/nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top " style="background-color: rgba(255,64,255,0.2) ">
+    <a class="navbar-brand" href="#">
+        <img src="<?= base_url('assets/images/stta.png') ?>" class="img-fluid" style="width:40px;" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Departemen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
