e820b869765a28f4e146bdf52f7d7c275683f0ab
Vantri Simanjuntak <vantri_antonius@yahoo.co.id>
2020-12-02 00:07:44 +0700

    DB Real

--- application/logs/log-2020-12-01.php
+++ application/logs/log-2020-12-01.php
@@ -1,3 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2020-12-01 07:13:31 --> 404 Page Not Found: /index
+ERROR - 2020-12-01 17:50:36 --> 404 Page Not Found: /index
